5d6a3e6dcc27ee9a50f2a6a965eb368e0daaa539
JoeyShapiro <joeyashapiro@gmail.com>
2022-01-31 14:34:23 -0600

    setup server connection

---
+++ serverTest.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = 'localhost'
+PORT = 3333
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data)
+            conn.sendall(b'Hello Rust!!!')
